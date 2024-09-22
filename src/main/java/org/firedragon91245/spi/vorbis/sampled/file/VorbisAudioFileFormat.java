62136d4250070672af316fa1c5b62405fe0f6e1d
FireDragon91245 <rickvoss2@gmx.de>
2024-09-22 22:11:54 +0200

    Updated License & Copyright Hints

--- src/main/java/org/firedragon91245/spi/vorbis/sampled/file/VorbisAudioFileFormat.java
+++ src/main/java/org/firedragon91245/spi/vorbis/sampled/file/VorbisAudioFileFormat.java
@@ -1,23 +1,23 @@
 /*
- *   VorbisAudioFileFormat.
- *
- *   JavaZOOM : vorbisspi@javazoom.net
- *              http://www.javazoom.net
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License as published
- *   by the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Library General Public License for more details.
- *
- *   You should have received a copy of the GNU Library General Public
- *   License along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+	original author: JavaZOOM, vorbisspi@javazoom.net, http://www.javazoom.net
+
+	vorbisspi-repackaged - a modernized and modularized version of vorbisspi by javazoom
+    Copyright (C) 2024 FireDragon91245
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
  */
 
 package org.firedragon91245.spi.vorbis.sampled.file;
