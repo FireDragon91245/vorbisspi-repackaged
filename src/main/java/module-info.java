0b0be155ea05b83ce3fea2279c1c98e5b6d56337
FireDragon91245 <rickvoss2@gmx.de>
2024-09-23 21:49:19 +0200

    Module Changes to allow Reflection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module vorbisspi.repackaged.main {
+open module vorbisspi.repackaged.main {
     requires java.desktop;
     requires tritonus.share;
     requires jorbis;
 
-    exports org.firedragon91245.spi;
+    exports org.firedragon91245.spi.vorbis.sampled.convert;
+    exports org.firedragon91245.spi.vorbis.sampled.file;
 
     provides javax.sound.sampled.spi.AudioFileReader with org.firedragon91245.spi.vorbis.sampled.file.VorbisAudioFileReader;
     provides javax.sound.sampled.spi.FormatConversionProvider with org.firedragon91245.spi.vorbis.sampled.convert.VorbisFormatConversionProvider;
