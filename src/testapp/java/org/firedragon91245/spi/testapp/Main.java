75a22dd1488e4234ea6406d6c6a7dba06f76a82d
FireDragon91245 <rickvoss2@gmx.de>
2024-09-23 17:13:00 +0200

    Added build for Test application

--- src/testapp/java/org/firedragon91245/spi/testapp/Main.java
+++ src/testapp/java/org/firedragon91245/spi/testapp/Main.java
@@ -5,12 +5,17 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        try(InputStream file = Main.class.getResourceAsStream("pigstep.ogg"))
+        try(InputStream file = Main.class.getResourceAsStream("/pigstep.ogg"))
         {
+            assert file != null;
             AudioInputStream s = AudioSystem.getAudioInputStream(file);
             System.out.println(s.getFormat().toString());
         }
